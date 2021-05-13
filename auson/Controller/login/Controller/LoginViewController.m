3b93927238701d7cdb35f18f0bd0a482d397209f
zhangtong <243816365@qq.com>
2021-05-13 18:49:28 +0800

    登录页面

--- auson/Controller/login/Controller/LoginViewController.m
+++ auson/Controller/login/Controller/LoginViewController.m
@@ -10,13 +10,19 @@
 #import "UILabel+YBAttributeTextTapAction.h"
 #import "ZTWebViewController.h"
 
-static NSString *agreementString = @"《auson用户协议》";
-static NSString *privacyPolicyString = @"《auson隐私政策》";
+static NSString *agreementString = @" 用户协议 ";
+static NSString *privacyPolicyString = @" 隐私政策 ";
 
-@interface LoginViewController ()<YBAttributeTapActionDelegate>
+@interface LoginViewController ()<YBAttributeTapActionDelegate,UITextFieldDelegate>
 
 @property (nonatomic, copy)       void(^complete)(BOOL success);
 @property (nonatomic, strong)     UILabel *agreementLabel;
+
+@property (nonatomic, strong)     UITextField *phoneTextField;
+@property (nonatomic, strong)     UITextField *smsCodeTextField;
+@property (nonatomic, strong)     UIButton *confromBtn;
+
+@property (nonatomic, strong)     UIStackView *stackView;
 
 @end
 
@@ -49,10 +55,29 @@ static NSString *privacyPolicyString = @"《auson隐私政策》";
     // Do any additional setup after loading the view.
     [self makeView];
     [self.view addSubview:self.agreementLabel];
+    [self.view addSubview:self.stackView];
     [_agreementLabel mas_makeConstraints:^(MASConstraintMaker *make) {
         make.bottom.mas_offset(-safeIphoneXFooter-30);
         make.centerX.mas_equalTo(0);
     }];
+    [_stackView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(RH(120));
+        make.width.mas_equalTo(RV(300));
+        make.centerX.mas_equalTo(0);
+    }];
+    
+    [_confromBtn mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(50);
+    }];
+    
+    [_phoneTextField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(50);
+    }];
+    
+    [_smsCodeTextField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.height.mas_equalTo(50);
+    }];
+    
 }
 
 - (void)makeView
@@ -60,18 +85,93 @@ static NSString *privacyPolicyString = @"《auson隐私政策》";
     [self setRightButtonTitle:@"随便看看" titleColor:GlobalColor];
 }
 #pragma mark - getter
+- (UIStackView *)stackView
+{
+    if (!_stackView) {
+        _stackView = [[UIStackView alloc] initWithArrangedSubviews:@[self.phoneTextField,self.smsCodeTextField,self.confromBtn]];
+        _smsCodeTextField.hidden = YES;
+        _stackView.spacing = 30;
+        _stackView.axis = UILayoutConstraintAxisVertical;
+    }
+    return _stackView;
+}
+- (UITextField *)phoneTextField
+{
+    if (!_phoneTextField) {
+        
+        UITextField *textfield = [UITextField new];
+        textfield.placeholder = @"请输入手机号";
+        textfield.font = [UIFont fontWithName:FDFONT_PINGFANGSC_SEMIBOLD size:15];
+        textfield.textColor = GTColor(@"#333333");
+        textfield.delegate = self;
+        textfield.keyboardType = UIKeyboardTypePhonePad;
+        textfield.clearButtonMode = UITextFieldViewModeWhileEditing;
+        UIView *line = [UIView new];
+        line.backgroundColor = GTColor(@"#f6f6f6");
+        [textfield addSubview:line];
+        [line mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.bottom.right.mas_equalTo(0);
+            make.height.mas_equalTo(1);
+        }];
+//        textfield.backgroundColor = GTColor(@"#f6f6f6");
+        
+        _phoneTextField = textfield;
+    }
+    return _phoneTextField;
+}
+
+- (UITextField *)smsCodeTextField
+{
+    if (!_smsCodeTextField) {
+        
+        UITextField *textfield = [UITextField new];
+        textfield.placeholder = @"请输入验证码";
+        textfield.font = [UIFont fontWithName:FDFONT_PINGFANGSC_SEMIBOLD size:15];
+        textfield.textColor = GTColor(@"#333333");
+        textfield.delegate = self;
+        textfield.keyboardType = UIKeyboardTypePhonePad;
+        textfield.clearButtonMode = UITextFieldViewModeWhileEditing;
+        UIView *line = [UIView new];
+        line.backgroundColor = GTColor(@"#f6f6f6");
+        [textfield addSubview:line];
+        [line mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.left.bottom.right.mas_equalTo(0);
+            make.height.mas_equalTo(1);
+        }];
+//        textfield.backgroundColor = GTColor(@"#f6f6f6");
+        
+        _smsCodeTextField = textfield;
+    }
+    return _smsCodeTextField;
+}
+- (UIButton *)confromBtn
+{
+    if (!_confromBtn) {
+        UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+        btn.backgroundColor = GlobalColor;
+        [btn setTitle:@"获取验证码" forState:UIControlStateNormal];
+        [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        btn.titleLabel.font = [UIFont fontWithName:FDFONT_PINGFANGSC_REGULAR size:15];
+        btn.layer.cornerRadius = 6;
+        [btn addTarget:self action:@selector(confromBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+        
+        _confromBtn = btn;
+    }
+    return _confromBtn;
+}
+
 - (UILabel *)agreementLabel
 {
     if (!_agreementLabel) {
         
-        NSString *string3 = @"登陆代表同意并愿意遵守";
+        NSString *string3 = @"登录既表明同意";
     
-        NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@\n%@和%@",string3,agreementString,privacyPolicyString]];
-        [attrString addAttribute:NSForegroundColorAttributeName value:GlobalColor range:NSMakeRange(string3.length+1, agreementString.length)];
-        [attrString addAttribute:NSForegroundColorAttributeName value:GlobalColor range:NSMakeRange(string3.length+1 + agreementString.length + 1, privacyPolicyString.length)];
+        NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"%@%@和%@",string3,agreementString,privacyPolicyString]];
+        [attrString addAttribute:NSForegroundColorAttributeName value:GTColor(@"#0e4b87") range:NSMakeRange(string3.length, agreementString.length)];
+        [attrString addAttribute:NSForegroundColorAttributeName value:GTColor(@"#0e4b87") range:NSMakeRange(string3.length + agreementString.length + 1, privacyPolicyString.length)];
         
         UILabel *label = [UILabel new];
-        label.font = [UIFont fontWithName:FDFONT_PINGFANGSC_REGULAR size:12];
+        label.font = [UIFont fontWithName:FDFONT_PINGFANGSC_REGULAR size:9];
         label.textColor = GTColor(@"#333333");
         label.textAlignment = NSTextAlignmentCenter;
         label.numberOfLines = 0;
@@ -97,6 +197,10 @@ static NSString *privacyPolicyString = @"《auson隐私政策》";
 - (void)rightButtonClick:(UIButton *)sender
 {
     [self dismissWithLoginSuccess:NO];
+}
+- (void)confromBtnClick:(UIButton *)sender
+{
+    NSLog(@"下一步");
 }
 
 - (void)dismissWithLoginSuccess:(BOOL)loginSuccess
